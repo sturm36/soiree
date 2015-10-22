3b883df6758b6a10c0026415cb97b868667da32d
Nishant Sharma <sharma.nishant159@gmail.com>
2015-10-22 22:27:57 +0000

    user association with pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,12 +7,12 @@ class PinsController < ApplicationController
     end 
     
     def new
-        @pin = Pin.new 
+        @pin = current_user.pins.build
         
     end 
     
     def create   
-        @pin = Pin.new(pin_params)
+        @pin = current_user.pins.build(pin_params)
         if @pin.save!
             puts @pin
             redirect_to @pin, notice: "Successfully Submitted the Pin"
